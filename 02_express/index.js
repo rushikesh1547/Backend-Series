543562fc8fa27556459e9e422953c9a2fd9bf549
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-01-13 23:41:51 +0530

    added morgan, winston in logger.js

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,35 +1,45 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 
 const port =process.env.PORT || 3000
 
-// app.get('/' , (req,res) => {
-//     res.send("Hello from Rishi!")
-// });
-
-// app.get('/ice-tea' , (req,res) => {
-//     res.send("What kind of Ice - Tea would you prefer?")
-// })
-
-// app.get('/twitter' , (req,res) => {
-//     res.send("rishidotcom")
-// })
-
 app.use(express.json())
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = []
 let nextId = 1
 //add a new tea
 app.post('/teas', (req,res) => {
-    
-    const {name,price}= req.body
+    const {name,price}= req.body;
     const newTea = {id: nextId++, name, price}
     teaData.push(newTea)
     res.status(201).send(newTea)
 })
+
+//get all tea
 app.get('/teas', (req,res) => {
+
     res.status(200).send(teaData)
 })
 
